d5b75172c5c2898671c4377643a2a0fe5f399aff
dlebansais <dlebansais@gmail.com>
2019-07-16 22:42:52 +0200

    Add project files.

---
+++ FormattedNumber/FormattedNumber.cs
@@ -0,0 +1,374 @@
+﻿namespace FormattedNumber
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    /// <summary>
+    /// Base interface for a number format that can parse any string.
+    /// </summary>
+    public interface IFormattedNumber
+    {
+        /// <summary>
+        /// Gets the significand part of the formatted number. Can be empty.
+        /// </summary>
+        string SignificandString { get; }
+
+        /// <summary>
+        /// Gets the exponent part of the formatted number. Can be empty.
+        /// </summary>
+        string ExponentString { get; }
+
+        /// <summary>
+        /// The trailing invalid text, if any.
+        /// </summary>
+        string InvalidText { get; }
+
+        /// <summary>
+        /// The canonical form of the parsed number.
+        /// </summary>
+        ICanonicalNumber Canonical { get; }
+    }
+
+    /// <summary>
+    /// Base interface for a number format that can parse any string.
+    /// </summary>
+    public abstract class FormattedNumber : IFormattedNumber
+    {
+        #region Constants
+        /// <summary>
+        /// The decimal separator in numeric values. The dot (unicode U+002E) is also always considered a valid separator.
+        /// </summary>
+        public static readonly string DecimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        #endregion
+
+        #region Init
+        /// <summary>
+        /// Parses a string to a formatted number.
+        /// </summary>
+        /// <param name="text">The string to parse.</param>
+        /// <param name="isLeadingZeroSupported">True if <paramref name="text"/> might have leading zeroes.</param>
+        /// <exception cref="NullReferenceException"><paramref name="text"/> is null.</exception>
+        /// <returns>The parsed number upon return.</returns>
+        public static IFormattedNumber Parse(string text, bool isLeadingZeroSupported = true)
+        {
+            if (text == null)
+                throw new NullReferenceException(nameof(text));
+
+            if (isLeadingZeroSupported)
+            {
+                // Eliminate leading zeroes.
+                while (text.Length > 1 && text[0] == '0')
+                    text = text.Substring(1);
+            }
+
+            IFormattedNumber Result = null;
+            bool Success = false;
+
+            if (!Success)
+                Success = TryParseAsIntegerWithBase(text, IntegerBase.Hexadecimal, out Result);
+            if (!Success)
+                Success = TryParseAsIntegerWithBase(text, IntegerBase.Octal, out Result);
+            if (!Success)
+                Success = TryParseAsIntegerWithBase(text, IntegerBase.Binary, out Result);
+            if (!Success)
+                Result = ParseAsNumber(text);
+
+            Debug.Assert(Result != null);
+            return Result;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormattedNumber"/> class.
+        /// </summary>
+        /// <param name="invalidText">The trailing invalid text, if any.</param>
+        /// <param name="canonical">The canonical form of the number.</param>
+        /// <exception cref="NullReferenceException"><paramref name="invalidText"/> or <paramref name="canonical"/> is null.</exception>
+        public FormattedNumber(string invalidText, ICanonicalNumber canonical)
+        {
+            InvalidText = invalidText ?? throw new NullReferenceException(nameof(invalidText));
+            Canonical = canonical ?? throw new NullReferenceException(nameof(canonical));
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the significand part of the formatted number. Can be empty.
+        /// </summary>
+        public abstract string SignificandString { get; }
+
+        /// <summary>
+        /// Gets the exponent part of the formatted number. Can be empty.
+        /// </summary>
+        public abstract string ExponentString { get; }
+
+        /// <summary>
+        /// The trailing invalid text, if any.
+        /// </summary>
+        public string InvalidText { get; }
+
+        /// <summary>
+        /// The canonical form of the parsed number.
+        /// </summary>
+        public ICanonicalNumber Canonical { get; }
+        #endregion
+
+        #region Implementation
+        /// <summary>
+        /// Tries to parse a string as an integer of the given base.
+        /// </summary>
+        /// <param name="text">The string to parse.</param>
+        /// <param name="integerBase">The integer base.</param>
+        /// <param name="number">The parsed number upon return, null on failure.</param>
+        /// <returns>True if the string was parsed successfully; Otherwise, false.</returns>
+        /// <exception cref="NullReferenceException"><paramref name="text"/> or <paramref name="integerBase"/> is null.</exception>
+        private protected static bool TryParseAsIntegerWithBase(string text, IIntegerBase integerBase, out IFormattedNumber number)
+        {
+            if (text == null)
+                throw new NullReferenceException(nameof(text));
+            if (integerBase == null)
+                throw new NullReferenceException(nameof(integerBase));
+
+            number = null;
+            bool IsParsed = false;
+
+            if (integerBase.Suffix != null && text.EndsWith(integerBase.Suffix))
+            {
+                ParseAsIntegerWithBase(text, integerBase, out number);
+                IsParsed = true;
+            }
+
+            return IsParsed;
+        }
+
+        /// <summary>
+        /// Parse a string as an integer of the given base.
+        /// </summary>
+        /// <param name="text">The string to parse. It must end with the suffix of <paramref name="integerBase"/>.</param>
+        /// <param name="integerBase">The integer base.</param>
+        /// <param name="number">The parsed number upon return, null on failure.</param>
+        private protected static void ParseAsIntegerWithBase(string text, IIntegerBase integerBase, out IFormattedNumber number)
+        {
+            Debug.Assert(text != null);
+            Debug.Assert(integerBase != null);
+
+            string Suffix = integerBase.Suffix;
+
+            Debug.Assert(Suffix == null || (Suffix.Length <= text.Length && text.EndsWith(integerBase.Suffix)));
+            string DigitText = Suffix == null ? text : text.Substring(0, text.Length - integerBase.Suffix.Length);
+
+            int i;
+            for (i = 0; i < DigitText.Length; i++)
+            {
+                int DigitValue;
+                if (!integerBase.IsValidDigit(DigitText[i], out DigitValue))
+                    break;
+            }
+
+            Debug.Assert(i <= text.Length);
+
+            string ValidText = DigitText.Substring(0, i);
+            string InvalidText = DigitText.Substring(i);
+
+            // Convert to decimal. The result can start or finish with a zero
+            string DecimalSignificand = IntegerBase.Convert(ValidText, integerBase, IntegerBase.Decimal);
+
+            // Eliminate starting zeroes.
+            while (DecimalSignificand.Length > 1 && DecimalSignificand[0] == '0')
+                DecimalSignificand = DecimalSignificand.Substring(1);
+
+            // Eliminate ending zeroes. Don't forget they are significant.
+            int BaseExponent = 0;
+            while (DecimalSignificand.Length > 1 && DecimalSignificand[DecimalSignificand.Length - 1] == '0')
+            {
+                DecimalSignificand = DecimalSignificand.Substring(0, DecimalSignificand.Length - 1);
+                BaseExponent++;
+            }
+            IntegerBase.Decimal.IsValidSignificand(DecimalSignificand);
+
+            string DecimalExponent = (BaseExponent + DecimalSignificand.Length - 1).ToString();
+
+            ICanonicalNumber Canonical = new CanonicalNumber(false, DecimalSignificand, false, DecimalExponent);
+
+            number = new IntegerNumberWithBase(ValidText, InvalidText, Canonical, integerBase);
+        }
+
+        /// <summary>
+        /// Parse a string as a number.
+        /// </summary>
+        /// <param name="text">The string to parse.</param>
+        /// <returns>
+        /// An instance of <see cref="IntegerNumber"/> if the valid part of the parsed number is an integer.
+        /// An instance of <see cref="RealNumber"/> if <paramref name="text"/> can be parsed, but not as an integer.
+        /// An instance of <see cref="InvalidNumber"/> if <paramref name="text"/> cannot be parsed as a number.
+        /// </returns>
+        private protected static IFormattedNumber ParseAsNumber(string text)
+        {
+            Debug.Assert(text != null);
+
+            // If empty, assume invalid.
+            if (text.Length == 0)
+                return new InvalidNumber(text);
+
+            // Assume '.' as the decimal separator in what follows. This means '.' is ALWAYS a valid separator.
+            text = text.Replace(DecimalSeparator, ".");
+
+            bool IsNegative = text[0] == '-';
+            int DigitStart = IsNegative ? 1 : 0;
+
+            // Reject numbers that don't start with a digit.
+            int DigitValue;
+            if (!IntegerBase.Decimal.IsValidDigit(text[DigitStart], out DigitValue))
+                return new InvalidNumber(text);
+
+            // If the first digit is 0, only accept the number zero.
+            if (DigitValue == 0)
+                if (IsNegative)
+                    return new InvalidNumber(text);
+                else
+                {
+                    Debug.Assert(DigitStart == 0);
+                    return new IntegerNumber(IntegerBase.Zero, text.Substring(DigitStart + 1), CanonicalNumber.Zero);
+                }
+
+            string ValidText;
+            string InvalidText;
+            string DecimalExponent;
+            string SignificandText;
+            ICanonicalNumber Canonical;
+
+            // Collect all digits before the decimal separator.
+            int n = DigitStart + 1;
+            while (n < text.Length && IntegerBase.Decimal.IsValidDigit(text[n], out DigitValue))
+                n++;
+
+            string IntegerText = text.Substring(DigitStart, n - DigitStart);
+            Debug.Assert(IntegerText.Length > 0 && IntegerText[0] != '0');
+
+            // If the number is a simple decimal number or continues with an invalid character, return now.
+            if (n >= text.Length || text[n] != '.')
+            {
+                InvalidText = text.Substring(n);
+                DecimalExponent = (n - 1).ToString();
+                Canonical = new CanonicalNumber(IsNegative, TrailingZeroesRemoved(IntegerText), false, DecimalExponent);
+                return new IntegerNumber(IntegerText, InvalidText, Canonical);
+            }
+
+            n++;
+
+            // Get the fractional part.
+            int FractionalBegin = n;
+            while (n < text.Length && IntegerBase.Decimal.IsValidDigit(text[n], out DigitValue))
+                n++;
+
+            string FractionalText = text.Substring(DigitStart + IntegerText.Length + 1, n - IntegerText.Length - 1 - DigitStart);
+
+            // If the fractional part is empty, or ends with a zero but is not zero, just return the integer.
+            if (FractionalText.Length == 0 || FractionalText[FractionalText.Length - 1] == '0')
+            {
+                ValidText = IntegerText;
+                InvalidText = text.Substring(DigitStart + ValidText.Length);
+                DecimalExponent = (IntegerText.Length - 1).ToString();
+                Canonical = new CanonicalNumber(IsNegative, TrailingZeroesRemoved(ValidText), false, DecimalExponent);
+                return new IntegerNumber(ValidText, InvalidText, Canonical);
+            }
+
+            // If the fractional part is not followed by an exponent, return the corresponding real number that has no exponent.
+            if (n == text.Length || (text[n] != 'e' && text[n] != 'E'))
+            {
+                InvalidText = text.Substring(n);
+                SignificandText = IntegerText + FractionalText;
+                DecimalExponent = (IntegerText.Length - 1).ToString();
+                return new RealNumber(IntegerText, FractionalText, ExplicitExponents.None, string.Empty, InvalidText, new CanonicalNumber(IsNegative, SignificandText, false, DecimalExponent));
+            }
+
+            Debug.Assert(FractionalText.Length > 0);
+            Debug.Assert(FractionalText == IntegerBase.Zero || FractionalText[FractionalText.Length - 1] != '0');
+            Debug.Assert(n < text.Length && (text[n] == 'e' || text[n] == 'E'));
+
+            // When an expontent is present, the mantissa part must have exactly one digit on the left side. We already know it's not zero.
+            if (IntegerText.Length != 1)
+            {
+                ValidText = text.Substring(DigitStart, 1);
+                InvalidText = text.Substring(DigitStart + ValidText.Length);
+                return new RealNumber(ValidText, string.Empty, ExplicitExponents.None, string.Empty, InvalidText, new CanonicalNumber(IsNegative, ValidText, false, IntegerBase.Zero));
+            }
+
+            SignificandText = TrailingZeroesRemoved(IntegerText + FractionalText);
+
+            int MantissaEnd = n;
+            n++;
+
+            ExplicitExponents ExplicitExponent;
+
+            // Allow exponent sign.
+            if (n + 1 < text.Length)
+            {
+                if (text[n] == '-')
+                {
+                    ExplicitExponent = ExplicitExponents.Negative;
+                    n++;
+                }
+
+                else if (text[n] == '+')
+                {
+                    ExplicitExponent = ExplicitExponents.Positive;
+                    n++;
+                }
+
+                else
+                    ExplicitExponent = ExplicitExponents.None;
+            }
+            else
+                ExplicitExponent = ExplicitExponents.None;
+
+            // Get the exponent.
+            int ExponentBegin = n;
+            while (n < text.Length && IntegerBase.Decimal.IsValidDigit(text[n], out DigitValue))
+                n++;
+            int ExponentEnd = n;
+
+            string ExponentText = text.Substring(ExponentBegin, ExponentEnd - ExponentBegin);
+
+            // The exponent cannot be empty, and must not start with zero. In that case, the valid part ends at the exponent character.
+            if (ExponentText.Length == 0 || ExponentText[0] == '0')
+            {
+                ValidText = text.Substring(DigitStart, MantissaEnd);
+                InvalidText = text.Substring(DigitStart + ValidText.Length);
+                DecimalExponent = (IntegerText.Length - 1).ToString();
+                return new RealNumber(IntegerText, FractionalText, ExplicitExponents.None, string.Empty, InvalidText, new CanonicalNumber(IsNegative, SignificandText, false, DecimalExponent));
+            }
+
+            // A number with a valid mantissa and explicit exponent.
+            ValidText = text.Substring(DigitStart, ExponentEnd);
+            InvalidText = text.Substring(DigitStart + ValidText.Length);
+            return new RealNumber(IntegerText, FractionalText, ExplicitExponent, ExponentText, InvalidText, new CanonicalNumber(IsNegative, SignificandText, ExplicitExponent == ExplicitExponents.Negative, ExponentText));
+        }
+
+        /// <summary>
+        /// Remove all zeroes at the end of a string. If the string is just made of zeroes, leave the last one untouched.
+        /// </summary>
+        /// <param name="text">The string to clean up.</param>
+        /// <returns>The string with zeroes removed.</returns>
+        private protected static string TrailingZeroesRemoved(string text)
+        {
+            Debug.Assert(text != null);
+
+            while (text.Length > 1 && text[text.Length - 1] == '0')
+                text = text.Substring(0, text.Length - 1);
+
+            return text;
+        }
+        #endregion
+
+        #region Debugging
+        /// <summary>
+        /// Returns the formatted number as a string.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"~{InvalidText} ({Canonical})";
+        }
+        #endregion
+    }
+}
---
+++ FormattedNumber/InvalidNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace FormattedNumber
+{
+    /// <summary>
+    /// The format for a number parsed as totally invalid.
+    /// </summary>
+    public class InvalidNumber : FormattedNumber
+    {
+        #region Init
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidNumber"/> class.
+        /// </summary>
+        /// <param name="invalidText">The invalid text.</param>
+        public InvalidNumber(string invalidText)
+            : base(invalidText, CanonicalNumber.Zero)
+        {
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the significand part of the formatted number.
+        /// </summary>
+        public override string SignificandString { get { return string.Empty; } }
+
+        /// <summary>
+        /// Gets the exponent part of the formatted number.
+        /// </summary>
+        public override string ExponentString { get { return string.Empty; } }
+        #endregion
+    }
+}
