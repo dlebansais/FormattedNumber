8e08ed4c2e99d3bafc9a7b0adff81aa9df7d68a5
dlebansais <dlebansais@gmail.com>
2019-09-01 11:17:46 +0200

    Added the coverity scan script. [skip ci]

---
+++ coverity_scan.bat
@@ -0,0 +1,10 @@
+set PATH=%PATH%;"C:\Applications\cov-analysis-win64-2019.03\bin"
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" /t:clean
+cov-build --dir cov-int "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild"
+
+C:\Applications\7-Zip\7z a cov-int cov-int -tzip
+
+curl --form token=RQ9mFrizWJLx9aApHCKqPQ --form email=dlebansais@gmail.com --form file=@/Projects/FormattedNumber/cov-int.zip "https://scan.coverity.com/builds?project=dlebansais/FormattedNumber"
+
+del cov-int.zip
