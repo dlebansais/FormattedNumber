8c245b4cbddd5f5cb7c9a5fdc48229ca19decd69
dlebansais <dlebansais@gmail.com>
2021-03-25 17:43:13 +0100

    Updated dependencies.

--- FormattedNumber/Properties/AssemblyInfo.cs
+++ FormattedNumber/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.208")]
-[assembly: AssemblyFileVersion("1.0.0.208")]
+[assembly: AssemblyVersion("1.0.0.209")]
+[assembly: AssemblyFileVersion("1.0.0.209")]
 [assembly: NeutralResourcesLanguage("en-US")]
